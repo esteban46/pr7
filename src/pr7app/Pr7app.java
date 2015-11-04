d73e8acdd725541729b94d13ba269c8e1f7e7641
Esteban <Esteban@EstebanPC>
2015-11-04 21:00:36 +0100

    Hola Alumnos de 2 DAW  2

--- src/pr7app/Pr7app.java
+++ src/pr7app/Pr7app.java
@@ -1,23 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package pr7app;
 
-/**
- *
- * @author ivan
- */
-public class Pr7app {
+import it.sauronsoftware.ftp4j.FTPAbortedException;
+import it.sauronsoftware.ftp4j.FTPClient;
+import it.sauronsoftware.ftp4j.FTPDataTransferException;
+import it.sauronsoftware.ftp4j.FTPException;
+import it.sauronsoftware.ftp4j.FTPIllegalReplyException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.io.InputStream; //entrada y salida 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+
+public class Pr7app {
+    public static void main(String[] args) throws MalformedURLException, IOException, IllegalStateException, FTPIllegalReplyException, FTPException, FileNotFoundException, FTPDataTransferException, FTPAbortedException {
         // TODO code application logic here
         
-        System.out.println("Hola profes2!");
+        URL url = new URL("http://192.168.122.200/docs/ic10-m04-WindowsServer.pdf");
+        url.openConnection();
+        InputStream reader = url.openStream();
+        FileOutputStream writer = new FileOutputStream("Esteban.pdf");
+        byte[] buffer = new byte[153600];
+        int bytesRead = 0;
+        while ((bytesRead = reader.read(buffer)) > 0) {
+            writer.write(buffer, 0, bytesRead);
+            buffer = new byte[153600];
+        }
+        writer.close();
+        reader.close();
+
+        FTPClient client = new FTPClient();
+        client.connect("srv.toca.cat");
+        client.login("fulano", "Platano123");
+        client.upload(new java.io.File("Esteban.pdf"));
+        client.disconnect(true);
     }
     
 }
